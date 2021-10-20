81ccca3b000f8f9b5521370fdadbda3c80c6d7fa
许松林 <xusonglin__1@163.com>
2021-10-20 15:50:55 +0800

    Initial commit

---
+++ src/test/java/com/examplejenkins/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplejenkins.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
